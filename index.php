1b5397199f2551e208b40f73e28d3bc5c887d48c
Nixon <canonalejandro17@gmail.com>
2023-06-29 19:34:05 -0500

    index nuevo

--- index.php
+++ index.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/inicioSesion.css">
+    <title>Iniciar sesion</title>
 </head>
 <body>
-    
+    <div name="content">
+        <div id="imagen">
+            <img src="img/user.png" width="250" height="250">
+        </div>
+        <div class="form">
+            <form action="" method="post">
+                <h1>Inicio de sesión</h1>
+                <h2>Para acceder a todas sus funciones</h2>
+                <Label>Usuario</Label>
+                <input type="text" name="txtusuario">
+                <Label>Contraseña</Label>
+                <input type="text" name="txtcontrasena">
+                <a href="Administrador/pages/editar.php">Contraseña olvidada?</a>
+                <input type="submit" value="Iniciar sesión">
+                <h2>No tiene cuenta?</h2>
+                <a href="Administrador/pages/agregar.php">Inscribirse</a>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
