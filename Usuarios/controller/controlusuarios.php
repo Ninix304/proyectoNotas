3d41e27d2181f61b73b76f05faabcf4db8cd5e7c
Nixon <canonalejandro17@gmail.com>
2023-07-12 19:43:55 -0500

    Control usuarios

---
+++ Usuarios/controller/controlusuarios.php
@@ -0,0 +1,18 @@
+<?php
+if($_POST["txtusuario"]=="Nixon Cañon" && $_POST["txtcontrasena"]=="123456"){
+    //crear la sesión
+    session_start();
+
+    //Definir variables de sesión
+    $_SESSION["usuario"]=$_POST["txtusuario"];
+    $_SESSION["validar"]=true;
+    $_SESSION["start"]=time();
+    $_SESSION["expire"]= $_SESSION["start"] + (1*60);
+    header("location: ../../inicio.php");
+}else{
+    print "<script>alert(\"Los datos que ingreso no son correctos, estupido\");window.location='../../index.php';</script>";
+}
+
+
+
+?>
